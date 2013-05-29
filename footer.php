77ebaff67ddd6ad6a023a8685abf1277e2cf5e3a
Chris Morata <chrismorata@gmail.com>
2013-05-29 10:45:05 -0400

    Update theme header and footer

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
-		<footer></footer>
+		<footer>&copy; <?php echo date('Y'); ?></footer>
 	</section>
+    
+    
+    <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php bloginfo('template_url');?>/_resources/js/libs/jquery-1.8.3.min.js"><\/script>')</script>
     	
-		<script defer src="<?php bloginfo('template_url');?>/js/plugins.js"></script>
-		<script defer src="<?php bloginfo('template_url');?>/js/script.js"></script>
+	<script defer src="<?php bloginfo('template_url');?>/js/plugins.js"></script>
+	<script defer src="<?php bloginfo('template_url');?>/js/script.js"></script>
 		
-		<!--[if lt IE 7 ]>
-		  <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-		  <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-		<![endif]-->
-		
-		<?php wp_footer(); ?>
+	<?php wp_footer(); ?>
 	
-	</body>
+</body>
 </html>
--- header.php
+++ header.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
-<!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]-->
-<!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]-->
-<!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]-->
-<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 
-	<head>
-		<meta charset="UTF-8" />
-		
-		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-		
-		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-		
-		<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-        
-		<?php wp_enqueue_script("jquery"); ?>
-		<?php wp_head(); ?>
-        
-  		<script src="<?php bloginfo('template_url');?>/resources/js/libs/modernizr-2.0.6.min.js"></script>
-        
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.js"></script>
-		<script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/resources/js/libs/jquery-1.6.2.min.js">\x3C/script>')</script>
-        
-	</head>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width" />
 	
-	<body <?php body_class(); ?>>
+	<title><?php wp_title('&laquo;', true, 'right'); ?></title>
 	
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	
+	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>    
+    
+	<script src="<?php bloginfo('template_url');?>/_resources/js/libs/modernizr-2.6.2.min.js"></script>
+    
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<!--[if lt IE 8]>
+        <div class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</div>
+    <![endif]-->
+	        
     <section id="container">
 		<header>
 			<?php wp_nav_menu( array('menu' => 'Main', 'container' => false, )); ?>
