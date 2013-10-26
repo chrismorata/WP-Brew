6ff2eb35d7988ca2e923a695a672d2e77fc875a4
Chris Morata <chrismorata@gmail.com>
2013-10-26 19:58:59 -0400

    Removing static jQuery from footer

--- footer.php
+++ footer.php
@@ -1,10 +1,5 @@
 		<footer>&copy; <?php echo date('Y'); ?></footer>
 	</section>
-    
-    
-    <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?php bloginfo('template_url');?>/_resources/js/libs/jquery-1.8.3.min.js"><\/script>')</script>
     	
 	<script defer src="<?php bloginfo('template_url');?>/js/plugins.js"></script>
 	<script defer src="<?php bloginfo('template_url');?>/js/script.js"></script>
